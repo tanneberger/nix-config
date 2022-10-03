22ce824b2dc7a15a1bb5793683c6912d45fce84f
revol-xut <revol-xut@protonmail.com>
2022-10-03 12:25:42 +0200

    add frontend for rmfakecloud

--- modules/server/rmfakecloud.nix
+++ modules/server/rmfakecloud.nix
@@ -1,5 +1,33 @@
-{ pkgs, config, lib, ... }: {
+{ pkgs, config, lib, mkYarnPackage, ... }: 
+let
 
+
+  source = pkgs.stdenv.mkDerivation rec {
+    name = "source-rmfakecloud";
+    version = "0.0.8";
+    src = pkgs.fetchFromGitHub {
+      owner = "ddvk";
+      repo = "rmfakecloud";
+      rev = "v${version}";
+      sha256 = "sha256-Q9zymQW8XWApy5ssfMtojN4AhteqrQzZyMeAkOsJDyw=";
+    };
+
+    phases = [ "unpackPhase" "installPhase" ];
+
+    installPhase = ''
+      mkdir -p $out/
+      cp -r ./ui/* $out/
+    '';
+  };
+  frontend = pkgs.mkYarnPackage rec {
+    pname = "rmfakecloud-frontend";
+    version = "0.0.8";
+    
+    src = source;
+
+  };
+
+in {
   services = {
     rmfakecloud = {
       enable = true;
@@ -17,6 +45,9 @@
             "/" = {
               proxyPass = "http://127.0.0.1:${toString config.services.rmfakecloud.port}/";
               proxyWebsockets = true;
+            };
+            "~.html" = {
+              root = "${frontend}/";
             };
           };
         };
