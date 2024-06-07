979464a56974fb3f39f29d618041e406b0a0a680
tanneberger <revol-xut@protonmail.com>
2024-06-07 22:18:05 +0200

    new bahn bingo version

--- modules/desktop/gnome.nix
+++ modules/desktop/gnome.nix
@@ -2,5 +2,5 @@
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.displayManager.gdm.wayland = true;
+  services.xserver.displayManager.gdm.wayland = false;
 }
