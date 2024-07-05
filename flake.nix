687bc13b5a01b87a29f677c965c59b6b6523ea85
tanneberger <revol-xut@protonmail.com>
2024-07-05 22:21:37 +0200

    updating nixos

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     sops-nix = {
@@ -48,7 +48,11 @@
       url = "github:icyphy/satellite-attitude-control";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixvim.url = "github:nix-community/nixvim/nixos-24.05";
+    nixvim = {
+      url = "github:nix-community/nixvim/nixos-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
     nvim = {
       url = "github:NuschtOS/nvim.nix";
       inputs.nixpkgs.follows = "nixpkgs";
